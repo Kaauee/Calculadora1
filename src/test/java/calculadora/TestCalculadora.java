76ea9669bec1e2a16e0e215db22aa3fd582c8c81
KaueLuiz21 <kaueluiz@outlook.com.br>
2022-05-25 14:37:25 -0300

    modificação para verificar workflow

--- src/test/java/calculadora/TestCalculadora.java
+++ src/test/java/calculadora/TestCalculadora.java
@@ -31,13 +31,7 @@ public class TestCalculadora {
         assertEquals(retornoEsperado, retornoFeito);
     }
     
-    @Test
-    public void testGetProduto() {
-        Calculadora calculadora = new Calculadora(4.0, 2.0);
-        double retornoEsperado = 8.0;
-        double retornoFeito = calculadora.getProduto();
-        assertEquals(retornoEsperado, retornoFeito);
-    }
+    
     
     @Test
     public void testGetDivisao() {
