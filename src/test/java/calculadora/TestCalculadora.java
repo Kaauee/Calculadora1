047c211d24c1ab4c1b70593336149fdf7dee310e
KaueLuiz21 <kaueluiz@outlook.com.br>
2022-05-25 14:27:05 -0300

    modificacoes

--- src/test/java/calculadora/TestCalculadora.java
+++ src/test/java/calculadora/TestCalculadora.java
@@ -4,12 +4,7 @@ package calculadora;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
 import calculadora.Calculadora;
 import static junit.framework.Assert.assertEquals;
 import org.junit.Test;
